c4f32e9b45838e58b3c366aacd6cff9b22a1a1a4
ppop <gegeyjn07@gmail.com>
2026-05-14 21:18:56 +0900

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,75 @@
 import './App.css'
+import { useState } from 'react'
+import ScoresTable from './components/ScoresTable'
+import style from './components/ScoresTable'
 
-const style = {
-  width: "500px",
-  height: "300px",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center"
+const intialScores = [
+  { 이름: "존", 국어: 90, 영어: 90, 수학: 80, 과학: 90 },
+  { 이름: "피터", 국어: 80, 영어: 80, 수학: 90, 과학: 80 },
+  { 이름: "수잔", 국어: 70, 영어: 70, 수학: 80, 과학: 70 },
+  { 이름: "수", 국어: 60, 영어: 60, 수학: 70, 과학: 60 },
+]
+
+const initialInfo = {
+  Name: "",
+  Korean: null,
+  English: null,
+  Math: null,
+  Science: null
 }
-
 
 function App() {
-  const handleChange = () =>{
+  const [info, setInfo] = useState(initialInfo)
+  const [scores,setScores] = useState(intialScores)
+
+  const handleChange = (event) => {
+    const {name,value} = event.target;
+    setInfo((prev) => (
+      { ...prev, [name]: value }
+    ))
     console.log(event.target.name)
   }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setScores((prev)=>([...prev, info]))
+    console.log(event.target)
+  }
   return (
-    <form>
-      <div>이름: 
-        <input 
-          type="text" 
-          name="이름" 
-          value=""
-          onChange={handleChange}
-          />
-      </div>
-      <div>국어: 
-        <input 
-          type="number" 
-          name="국어" 
-          value="" 
-          onChange={handleChange}
-        />
-      </div>
-      <div>영어: 
-        <input 
-          type="number" 
-          name="영어" 
-          value="" 
-          onChange={handleChange}
-        />
-      </div>
-      <div>수학: 
-        <input 
-          type="number" 
-          name="수학" 
-          value="" 
-          onChange={handleChange}
-          />
-      </div>
-      <div>과학: 
-        <input 
-          type="number" 
-          name="과학" 
-          value="" 
-          onChange={handleChange}
-        />
-        </div>
-      <button>제출</button>
+    <>
+    <ScoresTable scores={scores}/>
+    <form onSubmit={handleSubmit}>
+      <div>Name: <input
+        type="text"
+        name="Name"
+        value={info.Name}
+        onChange={handleChange} /></div>
+      <div>Korean: <input
+        type="number"
+        name="Korean"
+        value={info.Korean}
+        onChange={handleChange}
+      /></div>
+      <div>English: <input
+        type="number"
+        name="English"
+        value={info.English}
+        onChange={handleChange}
+      /></div>
+      <div>Math: <input
+        type="number"
+        name="Math"
+        value={info.Math}
+        onChange={handleChange}
+      /></div>
+      <div>Science: <input
+        type="number"
+        name="Science"
+        value={info.Science}
+        onChange={handleChange}
+      /></div>
+      <button>submit</button>
     </form>
+    </>
   )
 }
-
-export default App;
+export default App
--- src/components/ScoresTable.jsx
+++ src/components/ScoresTable.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
+import { useState } from 'react'
 
-const score = [
-  {이름:"plo",국어:90,수학:80,영어:90,과학:72},
-  {이름:"plk",국어:90,수학:80,영어:90,과학:72},
-  {이름:"plj",국어:90,수학:80,영어:90,과학:72},
-  {이름:"pli",국어:90,수학:80,영어:90,과학:72}  
+const scores = [
+  { 이름: "존", 국어: 90, 영어: 90, 수학: 80, 과학: 90 },
+  { 이름: "피터", 국어: 80, 영어: 80, 수학: 90, 과학: 80 },
+  { 이름: "수잔", 국어: 70, 영어: 70, 수학: 80, 과학: 70 },
+  { 이름: "수", 국어: 60, 영어: 60, 수학: 70, 과학: 60 },
 ]
+const style = {
+  width: "500px",
+  height: "300px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+}
 
-function ScoresTable() {
+function ScoresTable({scores}) {
   return (
-        <table>
-            <tr>
+    <table>
+      <tr>
         {
-          Object.keys(score[0]).map(key=>(
-          <th>{key}</th>
-        ))
-        }
-        
-      </tr>
-
-      {score.map(item => 
-       (
-       <tr>
-        {
-          Object.values(item).map(
-            value => (<td>{value}</td>)
-          )
+          Object.keys(scores[0]).map(key=>(
+            <th>{key}</th>
+          ))
         }
       </tr>
-      )
-    )
-  }
+      {scores.map(item => (
+          <tr>
+            {Object.values(item).map(
+              value=><td>{value}</td>
+            )}
+          </tr>
+        )
+      )}
     </table>
   )
 }
-
 export default ScoresTable
